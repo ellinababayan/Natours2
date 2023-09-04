b2bce804d6fe1f9dc8ab1950ef258a79d98f1632
Ellina Babaian <famogl375@gmail.com>
2023-09-03 18:20:47 -0700

    Features Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Home.css";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/NavBar";
+import About from "../../components/about/About";
+import Features from "../../components/features/Features";
 
 const Home = (props) => {
   return (
@@ -10,61 +12,9 @@ const Home = (props) => {
       <Header />
 
       <main>
-        <section class="section-about" id="section-about">
-          <div class="u-center-text u-margin-bottom-big">
-            <h2 class="heading-secondary">
-              Exciting tours for adventurous people
-            </h2>
-          </div>
-
-          <div class="row">
-            <div class="col-1-of-2">
-              <h3 class="heading-tertiary u-margin-bottom-small">
-                You`re going to fall in love with nature
-              </h3>
-              <p class="paragraph">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
-                voluptatum maiores, illum vel non minus. Fugit, quidem! Soluta
-                adipisci suscipit aspernatur hic, velit, minima corrupti vero
-                tenetur dolore, facilis accusantium?
-              </p>
-
-              <h3 class="heading-tertiary u-margin-bottom-small">
-                Live adventures like you never have before
-              </h3>
-              <p class="paragraph">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
-                voluptatum maiores, illum vel non minus. Fugit, quidem! Soluta
-                adipisci suscipit aspernatur hic, velit, minima corrupti vero
-                tenetur dolore, facilis accusantium?
-              </p>
-              <a href="#" class="btn-text">
-                Learn More &rarr;
-              </a>
-            </div>
-            <div class="col-1-of-2">
-              <div class="composition">
-                <img
-                  src="img/forest.jpg"
-                  alt="Photo 1"
-                  class="composition__photo composition__photo--p1"
-                />
-                <img
-                  src="img/mountain.jpg"
-                  alt="Photo 2"
-                  class="composition__photo composition__photo--p2"
-                />
-                <img
-                  src="img/waterfall.jpg"
-                  alt="Photo 3"
-                  class="composition__photo composition__photo--p3"
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section class="section-features" id="section-features">
+        <About />
+        <Features />
+        {/* <section class="section-features" id="section-features">
           <div class="row">
             <div class="col-1-of-4">
               <div class="feature-box">
@@ -115,7 +65,7 @@ const Home = (props) => {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         <section class="section-tours" id="section-tours">
           <div class="u-center-text u-margin-bottom-big">
